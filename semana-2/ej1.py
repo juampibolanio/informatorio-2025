02b2486152b071a2d044f59137bb1274b55faee8
Juan Pablo Bolanio <163171033+juampibolanio@users.noreply.github.com>
2025-07-09 10:53:43 -0300

    Ejercicio de semana 2

---
+++ semana-2/ej1.py
@@ -0,0 +1,14 @@
+#Ejercicio 1: Información Personal 
+#Instrucciones:
+#Crea una variable llamada nombre y asígnale tu nombre como un string.
+#Crea una variable llamada edad y asígnale tu edad como un número entero.
+#Crea una variable llamada altura y asígnale tu altura en metros como un número decimal (float).
+#Crea una variable llamada es_estudiante y asígnale un valor booleano (True o False) que indique si eres o no estudiante.
+#Finalmente, imprime cada una de estas variables en la consola.
+
+nombre = 'Juan Pablo'
+edad = 19
+altura = 1.76
+es_estudiante = True
+
+print("hola mundo")
