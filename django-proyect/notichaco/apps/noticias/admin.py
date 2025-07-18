bd53a7ddf35ad02d2a49fbfea4da2bca8beceef6
Juan Pablo Bolanio <juampi360786@gmail.com>
2025-07-18 16:04:55 -0300

    Realización de models y migraciones. Mejora del panel de django

--- django-proyect/notichaco/apps/noticias/admin.py
+++ django-proyect/notichaco/apps/noticias/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Noticia, Categoria
+
+#Personalizaciones
+class NoticiaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'fecha', 'autor')
+
+    list_filter = ('fecha', 'autor', 'categoria')
+
+    search_fields = ('titulo', 'autor')
 
 # Register your models here.
+
+admin.site.register(Noticia, NoticiaAdmin)
+admin.site.register(Categoria)
--- django-proyect/notichaco/apps/noticias/models.py
+++ django-proyect/notichaco/apps/noticias/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    categoria_id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=30)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+class Noticia(models.Model):
+    noticia_id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=80) 
+    subtitulo = models.CharField(max_length= 150)
+    contenido = models.TextField()
+    fecha = models.DateField(auto_now_add=True)
+    autor = models.CharField(max_length=50, null=True)
+    imagen = models.ImageField()
+    categoria = models.ManyToManyField(Categoria)
+
+    def __str__(self):
+        return self.titulo
